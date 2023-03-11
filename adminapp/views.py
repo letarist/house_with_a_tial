d8c70533eafa3c33e27640bc9093c25db22c6ba3
letarist <pentegov_92@mail.ru>
2023-03-11 18:07:17 +0300

    adminapp full with cbv

--- adminapp/views.py
+++ adminapp/views.py
@@ -47,46 +47,38 @@ class DeleteType(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
             return self.request.user
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def edit_type(request, pk):
-    type_animal = get_object_or_404(TypeOfAnimal, pk=pk)
-    if request.method == "POST":
-        edit_type = CreateType(request.POST, request.FILES, instance=type_animal)
-        if edit_type.is_valid():
-            edit_type.save()
-            return HttpResponseRedirect(reverse("adminapp:show_animal", args=[pk]))
-    edit_type = CreateType(instance=type_animal)
-    context = {
-        "edit_type": edit_type,
-    }
-    return render(request, "adminapp/edit_type.html", context)
+class EditType(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = TypeOfAnimal
+    template_name = 'adminapp/edit_type.html'
+    fields = '__all__'
+    success_url = reverse_lazy('adminapp:show_animal', args=[0])
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def add_animal(request, pk):
-    type_animal = get_object_or_404(TypeOfAnimal, pk=pk)
-    if request.method == "POST":
-        create_form = CreateAnimal(request.POST, request.FILES)
-        if create_form.is_valid():
-            create_form.save()
-            return HttpResponseRedirect(reverse("adminapp:show_animal", args=[pk]))
-    else:
-        create_form = CreateAnimal(initial={'type': type_animal})
-    context = {"create_form": create_form, 'type_animal': type_animal}
-    return render(request, "adminapp/create_animal.html", context)
+class AddAnimal(UserPassesTestMixin, LoginRequiredMixin, CreateView):
+    model = Animal
+    template_name = 'adminapp/create_animal.html'
+    success_url = reverse_lazy('adminapp:show_animal', args=[0])
+    fields = ('name', 'age', 'short_description', 'description', 'type')
+
+    def get_initial(self):
+        return {'type': self.kwargs['pk']}
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return self.request.user
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def edit_animal(request, pk):
-    current_animal = get_object_or_404(Animal, pk=pk)
-    if request.method == "POST":
-        edit_form = EditAnimal(request.POST, request.FILES, instance=current_animal)
-        if edit_form.is_valid():
-            edit_form.save()
-            return HttpResponseRedirect(reverse("adminapp:show_animal", args=[current_animal.type.pk]))
-    edit_form = EditAnimal(instance=current_animal)
-    context = {"edit_form": edit_form}
-    return render(request, "adminapp/edit_animal.html", context)
+class EditAnimal(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Animal
+    template_name = 'adminapp/edit_animal.html'
+    fields = '__all__'
+    success_url = reverse_lazy('adminapp:show_animal', args=[0])
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
 
 class DeleteAnimal(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
@@ -104,40 +96,30 @@ class DeleteAnimal(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
         return self.request.user.is_superuser
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def show_animal(request, pk):
-    animal_types = TypeOfAnimal.objects.all()
-    if pk != 0:
-        animals_of_type = Animal.objects.filter(type=pk).order_by("name")
-    else:
-        animals_of_type = Animal.objects.all()
-        context = {
-            "animals_of_type": animals_of_type,
-            "animal_types": animal_types,
-        }
-        return render(request, "adminapp/show_all_animals.html", context)
-    type_of_animal = get_object_or_404(TypeOfAnimal, pk=pk)
-    context = {
-        "animals_of_type": animals_of_type,
-        "animal_types": animal_types,
-        "type_of_animal": type_of_animal,
-    }
-    return render(request, "adminapp/main_admin.html", context)
+class ShowAnimal(UserPassesTestMixin, LoginRequiredMixin, ListView):
+    model = Animal
+    template_name = 'adminapp/show_all_animals.html'
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super(ShowAnimal, self).get_context_data(**kwargs)
+        data['animal_types'] = TypeOfAnimal.objects.all()
+        if self.kwargs['pk'] != 0:
+            object_list = Animal.objects.filter(type=self.kwargs['pk'])
+            data['object_list'] = object_list
+        return data
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def add_user(request):
-    if request.method == "POST":
-        user_form = UserRegisterForm(request.POST, request.FILES)
-        if user_form.is_valid():
-            user_form.save()
-            return HttpResponseRedirect(reverse("adminapp:show_users"))
-    else:
-        user_form = UserRegisterForm()
-    context = {
-        "user_form": user_form,
-    }
-    return render(request, "adminapp/add_user.html", context)
+class AddUser(UserPassesTestMixin, LoginRequiredMixin, CreateView):
+    model = User
+    template_name = 'adminapp/add_user.html'
+    fields = ('username', 'password', 'email', 'avatar')
+    success_url = reverse_lazy('adminapp:show_animal', args=[0])
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
 
 class DeleteUser(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
@@ -155,10 +137,14 @@ class DeleteUser(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
         return self.request.user.is_superuser
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def show_users(request):
-    users = User.objects.all().order_by("-is_active", "username")
-    context = {
-        "users": users,
-    }
-    return render(request, "adminapp/show_users.html", context)
+class ShowUsers(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = User
+    template_name = 'adminapp/show_users.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super(ShowUsers, self).get_context_data(**kwargs)
+        data['object_list'] = User.objects.all().order_by("-is_active", "username")
+        return data
+
+    def test_func(self):
+        return self.request.user.is_superuser
